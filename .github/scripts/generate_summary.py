f01de4c45c45b0539395546dfa9890ec21a8cd09
Marco Monroy <marcomonroy@outlook.com>
2026-06-27 19:44:43 +0200

    Add SQL polling

--- .github/scripts/generate_summary.py
+++ .github/scripts/generate_summary.py
@@ -1,26 +1,63 @@
 import os
-import requests
+import time
 import json
+import requests
 
 HOST = os.environ["DATABRICKS_HOST"]
 TOKEN = os.environ["DATABRICKS_TOKEN"]
 WAREHOUSE = os.environ["DATABRICKS_WAREHOUSE_ID"]
 
-response = requests.post(
-    f"{HOST}/api/2.0/sql/statements/",
-    headers={
-        "Authorization": f"Bearer {TOKEN}",
-        "Content-Type": "application/json"
-    },
-    json={
-        "warehouse_id": WAREHOUSE,
-        "statement": "SELECT COUNT(*) AS total FROM bronze_fuel_prices"
-    }
-)
+HEADERS = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Content-Type": "application/json"
+}
 
-print(response.status_code)
-print(json.dumps(response.json(), indent=2))
 
-with open(os.environ["GITHUB_STEP_SUMMARY"], "a") as f:
-    f.write("# SQL Warehouse Test\n")
-    f.write("Consulta enviada correctamente.\n")
+def execute_sql(statement):
+    # Enviar consulta
+    response = requests.post(
+        f"{HOST}/api/2.0/sql/statements/",
+        headers=HEADERS,
+        json={
+            "warehouse_id": WAREHOUSE,
+            "statement": statement
+        }
+    )
+
+    response.raise_for_status()
+
+    data = response.json()
+    statement_id = data["statement_id"]
+
+    # Esperar a que termine
+    while True:
+
+        status = requests.get(
+            f"{HOST}/api/2.0/sql/statements/{statement_id}",
+            headers=HEADERS
+        )
+
+        status.raise_for_status()
+
+        result = status.json()
+
+        state = result["status"]["state"]
+
+        print("SQL State:", state)
+
+        if state == "SUCCEEDED":
+            return result
+
+        if state in ("FAILED", "CANCELED"):
+            raise Exception(json.dumps(result, indent=2))
+
+        time.sleep(2)
+        
+        
+        
+        result = execute_sql("""
+            SELECT COUNT(*) AS total
+            FROM bronze_fuel_prices
+            """)
+
+        print(json.dumps(result, indent=2))
